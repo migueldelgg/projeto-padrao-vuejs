0889cd40ee424cf3eeb54d23f2f7a0daa7a80046
Miguel <delgdevoleper@gmail.com>
2024-07-09 18:35:03 -0300

    diretivas em vue-js -> v-if, v-show

--- src/components/infoPessoa.vue
+++ src/components/infoPessoa.vue
@@ -1,14 +1,25 @@
 <template>
-  <p>Estou trabalhando no momento.</p>
+
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>estou em busca de novas oportunidades</p>
+
   <p>Utilizo as seguintes tecnologias:</p>
   <ul>
     <li>JavaScript</li>
     <li>PHP</li>
     <li>Python</li>
   </ul>
+  <p v-show="mostrar_email">Manda uma mensagem para: {{ email }}</p>
 </template>
 <script>
 export default {
   name: "Info",
+  data(){
+    return {
+      email: "miguel.delg2004@gmail.com",
+      esta_trabalhando: false,
+      mostrar_email: true
+    }
+  }
 };
 </script>
