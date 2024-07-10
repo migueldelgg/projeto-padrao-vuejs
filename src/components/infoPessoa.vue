53ea35d9a3834dd2fa17748e1420eb5e10804b02
Miguel <delgdevoleper@gmail.com>
2024-07-10 13:57:09 -0300

    Atributos dinâmicos

--- src/components/infoPessoa.vue
+++ src/components/infoPessoa.vue
@@ -1,5 +1,4 @@
 <template>
-
   <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
   <p v-else>estou em busca de novas oportunidades</p>
 
@@ -10,16 +9,27 @@
     <li>Python</li>
   </ul>
   <p v-show="mostrar_email">Manda uma mensagem para: {{ email }}</p>
+  <p>
+    para acessar meu portifolio basta clicar<a v-bind:href="meu_link" target="_blank"> aqui</a>
+  </p>
+  <Picture/>
 </template>
 <script>
+
+import Picture from './Picture.vue'
+
 export default {
   name: "Info",
-  data(){
+  data() {
     return {
       email: "miguel.delg2004@gmail.com",
       esta_trabalhando: false,
-      mostrar_email: true
-    }
+      mostrar_email: true,
+      meu_link: "https://github.com/migueldelgg",
+    };
+  },
+  components: {
+    Picture
   }
 };
 </script>
