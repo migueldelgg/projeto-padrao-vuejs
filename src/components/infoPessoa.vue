4b34ac9d661015218c2cdcff069ae3a62723197d
Miguel <delgdevoleper@gmail.com>
2024-07-10 15:34:19 -0300

    v-for sintaxe -> renderizacao de listas

--- src/components/infoPessoa.vue
+++ src/components/infoPessoa.vue
@@ -2,24 +2,34 @@
   <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
   <p v-else>estou em busca de novas oportunidades</p>
 
-  <p>Utilizo as seguintes tecnologias:</p>
+  <p>Utilizo as seguintes tecnologias para back-end:</p>
   <ul>
-    <li>JavaScript</li>
-    <li>PHP</li>
-    <li>Python</li>
+    <li v-for="(technology, index) in backend_technologies" v-bind:key="index">
+      {{ technology }}
+    </li>
   </ul>
+  <p>Utilizo as seguintes tecnologias para front-end:</p>
+  <ul>
+    <li v-bind:key="index" v-for="(technology, index) in frontend_technologies">
+      {{ technology.language }}</li>
+  </ul>
+
   <p class="teste">
-    para acessar meu portifolio basta clicar<a v-bind:href="meu_link" target="_blank"> aqui</a>
+    para acessar meu portifolio basta clicar<a
+      v-bind:href="meu_link"
+      target="_blank"
+    >
+      aqui</a
+    >
   </p>
   <div>
-    <button @click="showEmail">{{textoBotao}}</button>
+    <button @click="showEmail">{{ textoBotao }}</button>
     <p v-show="mostrar_email">Manda uma mensagem para: {{ email }}</p>
   </div>
-  <Picture/>
+  <Picture />
 </template>
 <script>
-
-import Picture from './Picture.vue'
+import Picture from "./Picture.vue";
 
 export default {
   name: "Info",
@@ -29,21 +39,27 @@ export default {
       esta_trabalhando: false,
       mostrar_email: false,
       meu_link: "https://github.com/migueldelgg",
-      textoBotao: 'Mostrar e-mail'
+      textoBotao: "Mostrar e-mail",
+      backend_technologies: ["Java", "Spring", "Python"],
+      frontend_technologies: [
+        {id: 1, language: 'Html'},
+        {id: 2, language: 'Css'},
+        {id: 3, language: 'Vue'}
+      ]
     };
   },
   components: {
-    Picture
+    Picture,
   },
   methods: {
-    showEmail(){
-      this.mostrar_email = !this.mostrar_email
-      if(this.mostrar_email == false){
-        this.textoBotao = 'Mostrar e-mail'
-      }else {
-        this.textoBotao = 'Esconder e-mail'
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email;
+      if (this.mostrar_email == false) {
+        this.textoBotao = "Mostrar e-mail";
+      } else {
+        this.textoBotao = "Esconder e-mail";
       }
-    }
-  }
+    },
+  },
 };
 </script>
